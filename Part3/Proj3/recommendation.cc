90c411f08b3a3edc90b06fc822bd66419a4d7db0
Apostolos Florakis <32836747+apof@users.noreply.github.com>
2018-12-31 13:54:32 +0200

    Add files via upload

--- Part3/Proj3/recommendation.cc
+++ Part3/Proj3/recommendation.cc
@@ -5,11 +5,12 @@ int main(int argc, char** argv){
 
 srand(time(NULL)); 
 
-string tweet_file = "../Datasets/tweets_dataset_small.csv";
+string tweet_file = "../Datasets/tweets_dataset_big.csv";
 string coin_file = "../Datasets/coins_queries.csv";
 string lex_file = "../Datasets/vader_lexicon.csv";
+string output_file = "../Datasets/my_dataset.csv";
 
-data_preprocessing(tweet_file,coin_file,lex_file);
+data_preprocessing(tweet_file,coin_file,lex_file,output_file);
 
 return 0;
 }
